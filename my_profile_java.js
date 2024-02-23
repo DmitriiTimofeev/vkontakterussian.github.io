ffc1a037047f5c5ccf3df5055d0ccc745b3f5925
DmitriiTimofeev <103777091+DmitriiTimofeev@users.noreply.github.com>
2024-02-23 19:34:07 +0300

    Add files via upload

---
+++ my_profile_java.js
@@ -0,0 +1,9 @@
+// function before(){
+//     document.getElementById('message')
+//     .innerHTML="Вы не подписаны";
+// }
+
+// function afterr(){
+//     document.getElementById('message')
+//     .innerHTML="Вы подписаны";
+// }
